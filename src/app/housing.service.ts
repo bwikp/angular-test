ce010368e7a0689f36e33f78e366a8983a274929
bwikp <zilpa.michel@gmail.com>
2024-09-06 23:26:37 +0200

    revenu

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -12,7 +12,85 @@ export class HousingService {
     console.log(totalRent)
      }
 
-  getTotalRent(monthlyRent:any,agencyFees:any) {
+  getTotalRentMonth(monthlyRent:any,agencyFees:any) {
      return   monthlyRent * parseFloat(`1.${agencyFees}`)
   }
+
+  getAgencyFee(monthlyRent:any,agencyFees:any){
+    return monthlyRent * parseFloat(`0.${agencyFees}`)
+  }
+  get3yearFeeTotal(monthlyRent:any,agencyfee:any){
+
+    let totalFee = 0
+    let i = 0
+        do {
+          totalFee += monthlyRent * parseFloat(`0.${agencyfee}`)
+          i++
+        } while (i < 12 )
+        
+        do {
+          totalFee += monthlyRent * parseFloat(`0.${agencyfee - 5}`)
+          i++
+        } while ( i < 24 )
+          do {
+            totalFee += monthlyRent * parseFloat(`0.${agencyfee - 10}`)
+            i++
+          } while ( i < 36 )
+        
+        return totalFee 
+  }
+  get3yearRentTotal(monthlyRent:any,agencyfee:any){
+
+    let totalFee = 0
+    let i = 0
+        do {
+          totalFee += monthlyRent * parseFloat(`1.${agencyfee}`)
+          i++
+        } while (i < 12 )
+        
+        do {
+          totalFee += monthlyRent * parseFloat(`1.${agencyfee - 5}`)
+          i++
+        } while ( i < 24 )
+          do {
+            totalFee += monthlyRent * parseFloat(`1.${agencyfee - 10}`)
+            i++
+          } while ( i < 36 )
+        
+        return totalFee 
+  }
+
+  getYear1Total(monthlyRent:any,agencyfee:any,year:any) {
+    let totalFee = 0
+    let i = 0
+    let j = 0
+    let k = 0
+      if (year == 1){
+       
+        do {
+          totalFee += monthlyRent * parseFloat(`1.${agencyfee}`)
+          i++
+        } while (i < 12 )
+          return totalFee
+      }
+      if ( year == 2){
+        do {
+          totalFee += monthlyRent * parseFloat(`1.${agencyfee - 5}`)
+          j++
+        } while (j < 12 )
+          return totalFee
+
+      }
+
+      if ( year == 3){
+        do {
+          totalFee += monthlyRent * parseFloat(`1.${agencyfee - 10}`)
+          k++
+        } while (k < 12 )
+          return totalFee
+      }
+      else {
+        return 0
+      }
+}
 }
--- src/app/housing/housing.component.ts
+++ src/app/housing/housing.component.ts
@@ -17,7 +17,13 @@ export class HousingComponent {
             agencyFees: new FormControl(0)
         })
 
-        total = 0
+        totalRentMonth = 0
+        agencyfee = 0
+        totalfee3year = 0
+        totalrent3year = 0
+        totalyear1 = 0
+        totalyear2 = 0
+        totalyear3 = 0
          submitHouseValues(){
           this.housingService.submitHouseValues(
             this.housingForm.value.buyingPrice ?? 0,
@@ -25,6 +31,35 @@ export class HousingComponent {
             this.housingForm.value.annualCharges ?? 0,
             this.housingForm.value.agencyFees ?? 0
           ) 
-        this.total =   this.housingService.getTotalRent(this.housingForm.value.monthlyRent,this.housingForm.value.agencyFees)
+          this.totalRentMonth =   this.housingService.getTotalRentMonth(
+                                  this.housingForm.value.monthlyRent,
+                                  this.housingForm.value.agencyFees)
+          this.agencyfee = this.housingService.getAgencyFee(
+                                  this.housingForm.value.monthlyRent,
+                                  this.housingForm.value.agencyFees
+          )
+          this.totalfee3year = this.housingService.get3yearFeeTotal(
+                                  this.housingForm.value.monthlyRent,
+                                  this.housingForm.value.agencyFees
+          )
+          this.totalrent3year = this.housingService.get3yearRentTotal(
+                                  this.housingForm.value.monthlyRent,
+                                  this.housingForm.value.agencyFees
+          )
+          this.totalyear1 = this.housingService.getYear1Total(
+                                  this.housingForm.value.monthlyRent,
+                                  this.housingForm.value.agencyFees,
+                                  1
+          )
+          this.totalyear2 = this.housingService.getYear1Total(
+                                  this.housingForm.value.monthlyRent,
+                                  this.housingForm.value.agencyFees,
+                                  2
+          )
+          this.totalyear3 = this.housingService.getYear1Total(
+                                  this.housingForm.value.monthlyRent,
+                                  this.housingForm.value.agencyFees,
+                                  3
+          )
       }
     }
